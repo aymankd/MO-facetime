9c65364f41d45f3450afef7a58438b1fc5161c00
aymankd <aymankaddioui@gmail.com>
2020-06-06 16:13:00 +0100

    update theme

--- themes.pixelstrap.com/chitchat/assets/js/tour/intro-init.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-jQuery(document).ready(function() {
-    var intro_start = {
-        init: function() {
-            introJs().start();
-        }
-    };
-    intro_start.init()
-});
